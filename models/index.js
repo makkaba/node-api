46095b4204db9ad4ddfd8459ad666bcfe0540967
Jeff Chung <drifterz303@gmail.com>
2017-11-20 22:28:08 +0900

    deprecated로 인한 STRING 연산자 오류를 제거한다

--- models/index.js
+++ models/index.js
@@ -1,14 +1,15 @@
 //index에서 통합해서 돌려주자
 const config = require('../config/secret').db;
 const Sequelize = require("sequelize");
-
-const sequelize = new Sequelize(
+const Op = Sequelize.Op;
+const connection = new Sequelize(
   config.database, // 데이터베이스 이름
   config.username, // 유저 명
   config.password, // 비밀번호
   {
-    'host': config.hostname, // 데이터베이스 호스트
-    'dialect': 'mysql' // 사용할 데이터베이스 종류
+    host: config.hostname, // 데이터베이스 호스트
+    dialect: 'mysql', // 사용할 데이터베이스 종류
+    operatorsAliases: Op,
   }
 );
 
